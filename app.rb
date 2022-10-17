84388169a2694a84d1b0daca81f6ec3db6966dda
thadeus <thadeusnyariki94@gmail.com>
2022-10-17 23:16:59 +0300

    Done assignment

---
+++ app.rb
@@ -0,0 +1,8 @@
+# Use puts to display the string "Hello World!"
+puts "Hello World!"
+
+# Use print to display the string "Pass this test, please."
+print "Pass this test, please."
+
+# Use p to display the array [1,2,3]
+p [1, 2, 3]
